1fd362ff32ccded696db1a13b888666096483c3e
de-evg <d.e.min@yandex.ru>
2020-08-07 10:06:58 +0300

    добавил проверку на ведущий ноль в датах

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -9,8 +9,16 @@ const messages = [
 ];
 
 const generateDate = (start, end) => {
-  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));  
-  return `${date.getFullYear()}/${date.getMonth()}/${date.getDay() + 1} ${date.getHours()}: ${date.getMinutes()}`;
+  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  const checkOnLeadingZero = (someDate) => {    
+    return someDate < 10 ? `0${someDate}` : someDate;
+  };
+  const year = date.getFullYear();
+  const month = checkOnLeadingZero(date.getMonth() + 1);
+  const day = checkOnLeadingZero(date.getDay() + 1);
+  const hours = checkOnLeadingZero(date.getHours());
+  const minutes = checkOnLeadingZero(date.getMinutes());
+  return `${year}/${month}/${day} ${hours}:${minutes}`;
 };
 
 const generateComment = () => {
